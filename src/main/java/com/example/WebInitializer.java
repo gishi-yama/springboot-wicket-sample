551e77346aa3b81a652fb76dff9b7dd0bd4b48da
Hiroto Yamakawa <cist.yamakawa@gmail.com>
2014-11-27 23:44:38 +0900

    Remove Lombok

--- src/main/java/com/example/WebInitializer.java
+++ src/main/java/com/example/WebInitializer.java
@@ -1,9 +1,8 @@
 package com.example;
 
+import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
-
-import lombok.val;
 
 import org.apache.wicket.protocol.http.WicketFilter;
 import org.apache.wicket.spring.SpringWebApplicationFactory;
@@ -15,7 +14,7 @@ public class WebInitializer implements ServletContextInitializer {
 
 	@Override
 	public void onStartup(ServletContext context) throws ServletException {
-		val filter = context.addFilter("wicket-filter", WicketFilter.class);
+		FilterRegistration filter = context.addFilter("wicket-filter", WicketFilter.class);
 		filter.setInitParameter(WicketFilter.APP_FACT_PARAM, SpringWebApplicationFactory.class.getName());
 		filter.setInitParameter("applicationBean", "wicketApplication");
 		filter.setInitParameter(WicketFilter.FILTER_MAPPING_PARAM, "/*");
