0712034d615339decf8f37c419b3d8a286e77f07
Hiroto Yamakawa <cist.yamakawa@gmail.com>
2014-11-28 13:27:43 +0900

    add serialVersionUID

--- src/main/java/com/example/page/FooPage.java
+++ src/main/java/com/example/page/FooPage.java
@@ -8,6 +8,7 @@ import org.apache.wicket.spring.injection.annot.SpringBean;
 import com.example.service.IBarService;
 
 public class FooPage extends WebPage {
+	private static final long serialVersionUID = 1765245004153443150L;
 	
 	@SpringBean
 	private IBarService barService;
