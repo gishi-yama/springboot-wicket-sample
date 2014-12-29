c9ff15686ab2ab6aa97784b68c7941aa0e0a971f
Hiroto Yamakawa <cist.yamakawa@gmail.com>
2014-12-29 14:39:38 +0900

    add pom.xml settings for building war file

--- src/main/java/com/example/WicketApplication.java
+++ src/main/java/com/example/WicketApplication.java
@@ -6,12 +6,16 @@ import org.apache.wicket.spring.injection.annot.SpringComponentInjector;
 import org.apache.wicket.util.crypt.CharEncoding;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
 
 import com.example.page.FooPage;
 
-@SpringBootApplication
+@Component
+@EnableAutoConfiguration
+@ComponentScan
 public class WicketApplication extends WebApplication {
 
 	@Autowired
