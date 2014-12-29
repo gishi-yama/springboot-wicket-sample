7652bd62b1f3595c8e068f63a64aa21e9cb035ab
Hiroto Yamakawa <cist.yamakawa@gmail.com>
2014-12-29 11:48:43 +0900

    enable enclosed test-case

--- src/test/java/com/example/page/FooPageTest.java
+++ src/test/java/com/example/page/FooPageTest.java
@@ -1,5 +1,4 @@
 package com.example.page;
-
 import static org.mockito.Mockito.*;
 
 import org.apache.wicket.spring.injection.annot.SpringComponentInjector;
@@ -7,8 +6,10 @@ import org.apache.wicket.spring.test.ApplicationContextMock;
 import org.apache.wicket.util.tester.WicketTester;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -16,34 +17,59 @@ import com.example.WebInitializer;
 import com.example.WicketApplication;
 import com.example.service.IBarService;
 
-
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = { WebInitializer.class, WicketApplication.class })
+@RunWith(Enclosed.class)
 public class FooPageTest {
+		
+	@RunWith(SpringJUnit4ClassRunner.class)
+	@DirtiesContext 
+	@ContextConfiguration(classes = { WebInitializer.class, WicketApplication.class })
+	public static class MockingTest {
 
-	@Autowired
-	private WicketApplication wicketApplication;
+		@Autowired
+		private WicketApplication wicketApplication;
+		private ApplicationContextMock applicationContextMock;
+		private WicketTester sut;
 
-	private ApplicationContextMock applicationContextMock;
+		@Before
+		public void setUp() throws Exception {
+			sut = new WicketTester(wicketApplication);
+			applicationContextMock = new ApplicationContextMock();
+			sut.getApplication().getComponentInstantiationListeners()
+					.add(new SpringComponentInjector(sut.getApplication(), applicationContextMock));
+			IBarService mock = mock(IBarService.class);
+			when(mock.createMessage()).thenReturn("it is mocked!");
+			applicationContextMock.putBean("barService", mock);
+		}
 
-	private WicketTester sut;
-
-	@Before
-	public void setUp() throws Exception {
-		sut = new WicketTester(wicketApplication);
-		applicationContextMock = new ApplicationContextMock();
-		sut.getApplication().getComponentInstantiationListeners()
-				.add(new SpringComponentInjector(sut.getApplication(), applicationContextMock));
-		IBarService mock = mock(IBarService.class);
-		when(mock.createMessage()).thenReturn("it is mocked!");
-		applicationContextMock.putBean("barService", mock);
+		@Test
+		public void test() {
+			sut.startPage(FooPage.class);
+			sut.assertRenderedPage(FooPage.class);
+			sut.assertLabel("label", "it is mocked!");
+		}
+		
 	}
 
-	@Test
-	public void test() {
-		sut.startPage(FooPage.class);
-		sut.assertRenderedPage(FooPage.class);
-		sut.assertLabel("label", "it is mocked!");
+	@RunWith(SpringJUnit4ClassRunner.class)
+	@DirtiesContext 
+	@ContextConfiguration(classes = { WebInitializer.class, WicketApplication.class })
+	public static class UnMockingTest {
+
+		@Autowired
+		private WicketApplication wicketApplication;
+		private WicketTester sut;
+
+		@Before
+		public void setUp() throws Exception {
+			sut = new WicketTester(wicketApplication);
+		}
+
+		@Test
+		public void test() {
+			sut.startPage(FooPage.class);
+			sut.assertRenderedPage(FooPage.class);
+			sut.assertLabel("label", "Hello, Spring Boot + Wicket!");
+		}
 	}
 
 }
