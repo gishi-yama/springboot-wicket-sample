29c09b7c044886f79b11238a99ac94559eaf2b0a
Hiroto Yamakawa <cist.yamakawa@gmail.com>
2016-02-04 20:49:11 +0900

    Enable wicketstuff-annotation

--- src/main/java/com/example/WicketApplication.java
+++ src/main/java/com/example/WicketApplication.java
@@ -2,12 +2,14 @@ package com.example;
 
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.protocol.http.WebApplication;
+import org.apache.wicket.resource.DynamicJQueryResourceReference;
 import org.apache.wicket.spring.injection.annot.SpringComponentInjector;
 import org.apache.wicket.util.crypt.CharEncoding;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.wicketstuff.annotation.scan.AnnotatedMountScanner;
 
 import com.example.page.FooPage;
 
@@ -32,10 +34,13 @@ public class WicketApplication extends WebApplication {
     getRequestCycleSettings().setResponseRequestEncoding(CharEncoding.UTF_8);
     getMarkupSettings().setDefaultMarkupEncoding(CharEncoding.UTF_8);
     getComponentInstantiationListeners().add(new SpringComponentInjector(this, applicationContext));
+    new AnnotatedMountScanner().scanPackage("com.example.page").mount(this);
+    getJavaScriptLibrarySettings().setJQueryReference(new DynamicJQueryResourceReference());
+    mountPages();
   }
 
   public void mountPages() {
-    mountPage("/FooPage", FooPage.class);
+//    mountPage("/FooPage", FooPage.class);
   }
 
 }
--- src/main/java/com/example/page/FooPage.java
+++ src/main/java/com/example/page/FooPage.java
@@ -4,9 +4,11 @@ import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.wicketstuff.annotation.mount.MountPath;
 
 import com.example.service.IBarService;
 
+@MountPath("FooPage")
 public class FooPage extends WebPage {
   private static final long serialVersionUID = 1765245004153443150L;
 
